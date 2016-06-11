e3ac12e6a6ea0aa111bf3fcc7493d712e1013adb
EdderSouza <eddye_souza@yahoo.com.br>
2016-06-11 14:25:21 -0300

    Loop e ajustes

---
+++ Loops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int res = N * i;
+                Console.WriteLine(string.Format("{0} x {1} = {2}",N.ToString(), i.ToString(),res.ToString()));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- Loops/Properties/AssemblyInfo.cs
+++ Loops/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("Loops")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("Loops")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("628a1dc2-d53e-439f-b6e3-6ddb9f3237e5")]
+[assembly: Guid("3fde16a1-94f9-42f8-99cc-c37a1d28a1e3")]
 
 // Version information for an assembly consists of the following four values:
 //
