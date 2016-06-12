837d19643f0095af903da3f6c0052aa63cd98f7b
EdderSouza <eddye_souza@yahoo.com.br>
2016-06-11 21:11:42 -0300

    LetsReview

---
+++ LetsReview/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> texts = new List<string>();
+           
+            int qtdCase = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < qtdCase; i++)
+            {
+                texts.Add(Console.ReadLine());   
+            }
+
+            foreach (var item in texts)
+            {
+                string textOdd = string.Empty;
+                string textEven = string.Empty;
+
+                for (int i = 0; i < item.Length; i++)
+                {
+                    if (i >= 0 && i % 2 == 0)
+                    {
+                        textOdd += item[i];
+                    }
+                    else
+                    {
+                        textEven += item[i];
+                    }
+                }
+
+                Console.WriteLine(string.Format("{0} {1}", textOdd, textEven));
+            }
+                       
+
+            Console.ReadKey();
+        }
+    }
+}
