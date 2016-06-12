60498c38756985df867196c52f51de9f367aa332
EdderSouza <eddye_souza@yahoo.com.br>
2016-06-11 21:37:31 -0300

    LetsReview

---
+++ Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+
+            Array.Reverse(arr);
+
+            string[] arrStr = arr.Select(x => x.ToString()).ToArray();
+            
+            Console.WriteLine(string.Join(" ", arrStr));
+        }
+    }
+}
